9668848c53a816eac445d828d193ac2dd5843f5e
alecsouzza <alex.souza.lucas7@gmail.com>
2024-04-24 15:09:12 -0300

    up

---
+++ variaveis/estudos/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis{
+    public static void main(String[] args) throws Exception {
+    // tipos de dados 
+    /*
+     * int  = 4 bytes
+     * byte = 1 byte
+     * short = 2 bytes
+     * float =  4 bytes
+     * double = 8 bytes 
+     * long = 8 bytes
+     */ 
+    
+    System.out.println();
+
+    }
+}
